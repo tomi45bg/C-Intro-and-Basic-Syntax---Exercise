21c785e39bd3c3650d9606de648351c37da465f6
tomi45bg <omftomas@gmail.com>
2023-05-14 11:33:06 +0300

    Add project files.

---
+++ Ages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            0 - 2 – baby
+            3 - 13 – child
+            14 - 19 – teenager
+            20 - 65 – adult
+            >= 66 – elder
+            */
+
+            int age = int.Parse(Console.ReadLine());
+            string sex = "";
+
+            if (age >= 0 && age <= 2)
+            {
+                sex = "baby";
+            } else if (age >= 3 && age <= 13)
+            {
+                sex = "child";
+            } else if (age > 13 && age <= 19)
+            {
+                sex = "teenager";
+            } else if (age >= 20 && age <= 65)
+            {
+                sex = "adult";
+            } else
+            {
+                sex = "elder";
+            }
+
+            Console.WriteLine(sex);
+        }
+    }
+}
