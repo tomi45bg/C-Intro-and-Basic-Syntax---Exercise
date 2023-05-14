ad248c1023bb4069b8cce54490556ce02baa1ba9
tomi45bg <omftomas@gmail.com>
2023-05-14 15:51:12 +0300

    done!

---
+++ Orders/Orders.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+            double price = 0;    
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double priceThisMonth = pricePerCapsule * days * capsulesCount;
+                price += priceThisMonth;
+                Console.WriteLine($"The price for the coffee is: ${priceThisMonth:f2}");
+            }
+
+            Console.WriteLine($"Total: ${price:f2}");
+        }
+    }
+}
