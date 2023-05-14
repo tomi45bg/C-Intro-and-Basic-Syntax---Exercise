10dde3c29bbd5bb5445e9736ad9e6df5c022ceab
tomi45bg <omftomas@gmail.com>
2023-05-14 15:42:56 +0300

    done!!

---
+++ Padawan Equipment/Padawan Equipment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float moneyJohn = float.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            float priceLightsaber = float.Parse(Console.ReadLine());
+            float priceRobe = float.Parse(Console.ReadLine());
+            float priceBelt = float.Parse(Console.ReadLine());
+
+            float moneyLightsabers = (float)(priceLightsaber * Math.Ceiling(studentsCount + 0.1 * studentsCount));
+
+            int count6Belts = 0;
+            for (int i = 1; i <= studentsCount; i++) {
+                if (i % 6 == 0)
+                {
+                    count6Belts++;
+                }
+            }
+
+            float money = moneyLightsabers + priceRobe*studentsCount + 
+                priceBelt*(studentsCount - count6Belts);
+
+            if (money <= moneyJohn)
+            {
+                Console.WriteLine($"The money is enough - it would cost {money:f2}lv.");
+            } else
+            {
+                float diff = Math.Abs(money - moneyJohn);
+                Console.WriteLine($"John will need {diff:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ Rage Expenses/Rage Expenses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetCrashes = 0;
+            int mouseCrashes = 0;
+            int keyboardCrashes = 0;
+            int displayCrashes = 0;
+
+            for (int i = 1; i <= lostCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    headsetCrashes++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    mouseCrashes++;
+                }
+
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    keyboardCrashes++;
+                    if (keyboardCrashes % 2 == 0)
+                    {
+                        displayCrashes++;
+                    }
+                }
+            }
+
+            double headset = headsetCrashes * headsetPrice;
+            double mouse = mouseCrashes * mousePrice;
+            double keyboard = keyboardCrashes * keyboardPrice;
+            double display = displayCrashes * displayPrice;
+
+            double priceAll = headset + mouse + keyboard + display;
+
+            Console.WriteLine($"Rage expenses: {priceAll:f2} lv.");
+        }
+    }
+}
