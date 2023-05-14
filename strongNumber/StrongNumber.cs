a90190f8f62be9e2f8cf208090d4bb5450f4d610
tomi45bg <omftomas@gmail.com>
2023-05-14 14:23:27 +0300

    done

---
+++ strongNumber/StrongNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace strongNumber
+{
+    internal class StrongNumber
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int originalNum = num;
+            bool isStrong = false;
+
+            int sum = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                int sumFact = 1;
+
+                for (int i = 1; i <= digit; i++)
+                {
+                    sumFact *= i;
+                }
+
+                sum += sumFact; 
+
+                num /= 10;
+            }
+
+            if (sum == originalNum)
+            {
+                isStrong = true;
+            }
+
+            if (isStrong)
+            {
+                Console.WriteLine("yes");
+            } else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
