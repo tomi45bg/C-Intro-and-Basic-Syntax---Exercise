6f372d8bf88eca73a2347a15b1b658f1f481c4cf
tomi45bg <omftomas@gmail.com>
2023-05-14 14:13:09 +0300

    update

---
+++ Login/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+
+            for(int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            int countError = 0;
+            string command = Console.ReadLine();
+            bool correct = true;
+
+            while(command != password)
+            {
+                countError++;
+                if (countError == 4) 
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+
+                correct = false;
+
+                Console.WriteLine("Incorrect password. Try again.");
+
+                command = Console.ReadLine();   
+
+                if (command == password) {
+                    correct = true;
+                }
+            }
+
+            if (correct)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
